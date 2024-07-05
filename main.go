55cba0825b0cfe8e1404acba1152872f8b15dbb1
Farshid Tavakolizadeh <email@farshid.ws>
2024-07-05 14:53:29 +0200

    Remove env injection logic

--- main.go
+++ main.go
@@ -6,37 +6,36 @@ import (
 )
 
 func check_env_vars(tests []string) {
-  for _, env := range tests{
-    if v, ok := os.LookupEnv(env); ok {
-      fmt.Printf("Found %s : %s\n", env, v)
-      continue
-    }
-    fmt.Printf("Environment variable %s not set\n", env)
-  }
+	for _, env := range tests {
+		if v, ok := os.LookupEnv(env); ok {
+			fmt.Printf("Found %s : %s\n", env, v)
+			continue
+		}
+		fmt.Printf("Environment variable %s not set\n", env)
+	}
 }
 
 func main() {
 	var tests = []string{
-    "PORT",
-    "SERVER",
-    "COFFE",
-    "CHECK",
-  }
+		"PORT",
+		"SERVER",
+		"COFFE",
+		"CHECK",
+	}
 
-  var envfile = []string{
-    "ORACULAR",
-    "NOBLE",
-    "MANTIC",
-    "LUNAR",
-    "KINETIC",
-    "JAMMY",
-  }
+	var envfile = []string{
+		"ORACULAR",
+		"NOBLE",
+		"MANTIC",
+		"LUNAR",
+		"KINETIC",
+		"JAMMY",
+	}
 
-  fmt.Printf("\nChecking for env variables:\n")
-  check_env_vars(tests)
+	fmt.Printf("\nChecking for env variables:\n")
+	check_env_vars(tests)
 
-  fmt.Printf("\nChecking for ENVFILE variables:\n")
-  check_env_vars(envfile)
-  os.Exit(0)
+	fmt.Printf("\nChecking for ENVFILE variables:\n")
+	check_env_vars(envfile)
+	os.Exit(0)
 }
-
