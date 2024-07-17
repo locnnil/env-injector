4b12abd71ed3cc8e961428230cdf5306337cef59
Farshid Tavakolizadeh <email@farshid.ws>
2024-07-17 12:05:50 +0200

    Manual exporting

---
+++ snap/local/bin/load-myapp1-options.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+debug="$(snapctl get debug)"
+if [ -z "$debug" ]; then
+  export DEBUG="$debug"
+fi
+
+port="$(snapctl get myapp1.port)"
+if [ -z "$port" ]; then
+  export PORT="$port"
+fi
+
+addr="$(snapctl get myapp1.bind-address)"
+if [ -z "$addr" ]; then
+  export BIND_ADDRESS="$addr"
+fi
+
+# < ... > 
+
+exec "$@"
